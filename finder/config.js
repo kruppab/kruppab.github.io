695bc611229c0463a384ee86b1f0febb429073c4
kruppab <kruppab@gmail.com>
2022-12-07 11:17:13 +0100

    radio

--- finder/config.js
+++ finder/config.js
@@ -40,7 +40,7 @@ const config = {
       ],
     },
     {
-      type: 'checkbox',
+      type: 'radio',
       title: 'Type: ',
       columnHeader: 'Type', // Case sensitive - must match spreadsheet entry
       listItems: ['compound feed producer',
